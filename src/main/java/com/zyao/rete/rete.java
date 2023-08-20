57cbf9eae205a7802cd6c75e9226cd712312ad85
1zyao <liuziyao20191212@163.com>
2023-08-20 20:39:49 +0800

    修改版本号

--- src/main/java/com/zyao/rete/rete.java
+++ src/main/java/com/zyao/rete/rete.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 public class rete {
     public static final String MODID = "rete";
     public static final String NAME = "RETE Server Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String GUIFACTORG = "com.zyao.rete.reteConfigGuiFactory";
 
     public rete() {
